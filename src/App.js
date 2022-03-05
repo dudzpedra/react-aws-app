4f759d19093736d203088245bfa1f1a091c204b8
Eduardo <eduardo@sanp.com.br>
2022-03-05 20:16:20 -0300

    added p tag with link to aws tutorial

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
         Github Repo: {' '}
         <a href='https://github.com/dudzpedra/react-aws-app' target='_blank' rel='noreferrer' >React AWS app</a>
       </p>
+      <p>
+        Learn more: {' '}
+        <a href='https://aws.amazon.com/pt/getting-started/hands-on/deploy-react-app-cicd-amplify/' target='_blank' rel='noreferrer' >Deploy a React app with AWS</a>
+      </p>
     </div>
   );
 }
