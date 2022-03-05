bfe023ed22c729ccf6f167dfab908704f3277933
Eduardo <eduardo@sanp.com.br>
2022-03-05 20:05:51 -0300

    first edit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>A simple React and AWS app.</h1>
+      <p>
+        Github Repo: {' '}
+        <a href='https://github.com/dudzpedra/react-aws-app' target='_blank' rel='noreferrer' >React AWS app</a>
+      </p>
+    </div>
+  );
+}
+
+export default App;
